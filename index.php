6973a9696e3341cd8e8c343010e9840af0c1d265
mariemeNDG <ndeyemariemesarrndong06@gmail.com>
2026-06-30 19:31:12 +0000

    definition d'un tableau categorie

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php 
+
+$categories = 
+[
+
+0=> [  'nom'=> "sac",
+       'code'=> "0EU8",
+       'produits'=> [
+                   0 => ['nom'=>"sac-a-main",
+                        'ref'=>"0ER5",
+                        'prix'=>8500,
+                        'quantite'=>500
+                        ],
+                   1 => ['nom'=>"sac-a-dos",
+                        'ref'=>"0DR5",
+                        'prix'=>6000,
+                        'quantite'=>700
+                        ]
+                    ],
+    ],
+
+1=> [  'nom'=> "Accessoirs",
+       'code'=> "0PU8",
+       'produits'=> []
+    ]
+];
+
+
+
+?>
