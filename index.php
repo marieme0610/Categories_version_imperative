a53fc40b47b10bdf629140b2ac17e9135d12da3a
mariemeNDG <ndeyemariemesarrndong06@gmail.com>
2026-07-01 00:26:19 +0000

    refactor: tab-Categorie2

--- index.php
+++ index.php
@@ -3,27 +3,27 @@
 $categories = [
 
    0 =>      [
-            "code" => "ERT5",
-            "nom" => "Parfum",
-            "produits" => [
+            'nom' => 'ERT5',
+            'code' => 'Parfum',
+            'produits' => [
                   0 => [
-                    "nom" => "victoriaSecret",
-                    "ref" => "555Y",
-                    "prix" => 15000,
-                    "quantite" => 284 
+                    'nom' => 'victoriaSecret',
+                    'ref' => '555Y',
+                    'prix' => 15000,
+                    'quantite' => 284 
                   ],
                   1 => [
-                    "nom" => "Love me",
-                    "ref" => "FG5R",
-                    "prix" => 1500,
-                    "quantite" => 76 
+                    'nom' => 'Love me',
+                    'ref' => 'FG5R',
+                    'prix' => 1500,
+                    'quantite' => 76 
                   ]
             ]
          ],
    1 =>      [
-            "code" => "A3E6",
-            "nom" => "soins-de-visage",
-            "produits" => []
+            'nom' => 'A3E6',
+            'code' => 'soins-de-visage',
+            'produits' => []
          ]
 ];
 
